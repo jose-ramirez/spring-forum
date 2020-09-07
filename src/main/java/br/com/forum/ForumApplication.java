7e1992363c1171848b29aff4316652917147fd42
Jose Ramirez <a30673083@gmail.com>
2020-09-07 20:44:51 -0300

    Caching

--- src/main/java/br/com/forum/ForumApplication.java
+++ src/main/java/br/com/forum/ForumApplication.java
@@ -2,10 +2,11 @@ package br.com.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ForumApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
